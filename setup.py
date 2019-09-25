4c96e80ccf1a8461202837738b642ba5e2ebd631
axsk <smartmind23@googlemail.com>
2019-09-25 15:11:43 +0200

    fix wrong setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cmdtools2",
+    name="cmdtools",
     version="0.0.1",
     author="Alexander Sikorski",
     author_email="sikorski@zib.de",
