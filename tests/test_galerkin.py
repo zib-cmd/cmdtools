bf73b7c0abf37b816f988cc21a16818a1dd8b504
Renata Sechi <sechi@zib.de>
2019-11-27 10:50:21 +0100

    fix test

--- tests/test_galerkin.py
+++ tests/test_galerkin.py
@@ -1,11 +1,11 @@
 from numpy.random import rand
 from cmdtools import utils
-from cmdtools.estimation import galerkin, find_bandwidth
+from cmdtools.estimation import galerkin
 
 
 def test_propagator():
     x = rand(10, 3)
     centers = rand(4, 3)
-    sigma = find_bandwidth(X, centers)
+    sigma = galerkin.find_bandwidth(x, centers)
     P = galerkin.propagator(x, centers, sigma)
     assert utils.is_rowstochastic(P)
