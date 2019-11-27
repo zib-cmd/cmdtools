499a509a583e6eace5a14c361fc7befe067b01cf
Renata Sechi <sechi@zib.de>
2019-11-27 10:34:14 +0100

    add test for find_bandwidth

--- tests/test_galerkin.py
+++ tests/test_galerkin.py
@@ -1,11 +1,11 @@
 from numpy.random import rand
 from cmdtools import utils
-from cmdtools.estimation import galerkin
+from cmdtools.estimation import galerkin, find_bandwidth
 
 
 def test_propagator():
     x = rand(10, 3)
     centers = rand(4, 3)
-    sigma = 1
+    sigma = find_bandwidth(X, centers)
     P = galerkin.propagator(x, centers, sigma)
     assert utils.is_rowstochastic(P)
