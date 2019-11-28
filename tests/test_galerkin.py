8d0914f93e42af05d65613e1fd3dfeb75a2b2d5a
axsk <smartmind23@googlemail.com>
2019-11-28 16:39:18 +0100

    add propagator test for identity

--- tests/test_galerkin.py
+++ tests/test_galerkin.py
@@ -1,3 +1,4 @@
+import numpy as np
 from numpy.random import rand
 from cmdtools import utils
 from cmdtools.estimation import galerkin
@@ -9,3 +10,10 @@ def test_propagator():
     traj = galerkin.Trajectory(timeseries=x, centers=centers)
     P = traj.propagator
     assert utils.is_rowstochastic(P)
+
+def test_propagator_identity():
+    x = np.ones([10,3])
+    centers = rand(4,3)
+    traj = galerkin.Trajectory(timeseries=x, centers=centers)
+    P = traj.propagator
+    assert np.isclose(P, np.identity(4)).all()
