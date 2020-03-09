4b1ff3beb56defd4f665f1a63959847fe05ddbeb
Renata Sechi <sechi@zib.de>
2020-03-09 11:51:07 +0100

    update

--- src/cmdtools/estimation/untitled0.py
+++ src/cmdtools/estimation/untitled0.py
@@ -1,18 +1,68 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Dec 18 11:19:09 2019
+Created on Thu Feb 27 14:15:51 2020
 
 @author: bzfsechi
-picking algorithm as explained in "Set-free Markov state model building "
 """
 import numpy as np
-
-def picking_algorithm( _min, _max, dim, n_final):
-    
-    start_set =(_max-_min)* np.random.random_sample((n_final, dim)) + _min
-    
-    return(start_set)
-
+import matplotlib.pyplot as plt
+from statistics import mean
+import scipy
 
 #%%
+# take mean of second eigenvalue error and put into a list
+def extract(arr):
+    eig1= np.sort(np.real(np.linalg.eigvals(arr[2])))[-2]
+#    print(eig1)
+    eig2= np.sort(np.real(np.linalg.eigvals(arr[3])))[-2]
+    print(eig1,eig2)
+    return(abs(abs(eig1-eig2)/eig1))
+    
+#mean_diff1 = []
+#for j in np.arange(10,200, step=10):
+#    file = np.load("test_newton_dim%d_b1.npz"%j, allow_pickle=True)
+#    snd_eigval = []
+#    for i in range(100):
+#        snd_eigval.append(extract(file["arr_%d"%i]))
+#    mean_diff1.append(mean(snd_eigval))
+mean_diff10 = []
+for j in np.arange(10,200, step=10):
+    file = np.load("test_deg_dim%d_b10.npz"%j, allow_pickle=True)
+    snd_eigval = []
+    for i in range(100):
+        snd_eigval.append(extract(file["arr_%d"%i]))
+    mean_diff10.append(mean(snd_eigval))
+#mean_diff15 = []
+#for j in np.arange(10,200, step=10):
+#    file = np.load("test_newton_dim%d_b15.npz"%j, allow_pickle=True)
+#    snd_eigval = []
+#    for i in range(100):
+#        snd_eigval.append(extract(file["arr_%d"%i]))
+#    mean_diff15.append(mean(snd_eigval))
+#mean_diff5 = []
+#for j in np.arange(10,200, step=10):
+#    file = np.load("test_newton_dim%d_b5.npz"%j, allow_pickle=True)
+#    snd_eigval = []
+#    for i in range(100):
+#        snd_eigval.append(extract(file["arr_%d"%i]))
+#    mean_diff5.append(mean(snd_eigval))
+#    #%%
+#mean_diff2 = []
+#for j in np.arange(10,200, step=10):
+#    file = np.load("test_newton_dim%d_b2.npz"%j, allow_pickle=True)
+#    snd_eigval = []
+#    for i in range(100):
+#        snd_eigval.append(extract(file["arr_%d"%i])[-2])
+#    mean_diff2.append(mean(snd_eigval))
+
+    #%%
+plt.scatter(np.arange(10,200, step=10), mean_diff1, label =1)
+plt.scatter(np.arange(10,200, step=10), mean_diff10,label =10)
+plt.scatter(np.arange(10,200, step=10), mean_diff5, label =5)
+plt.scatter(np.arange(10,200, step=10), mean_diff15, label =15)
+plt.xlabel('dim')
+plt.ylabel('($\lambda_i-\lambda_f)$')
+#plt.xlim(30,200)
+plt.ylim(0)
+plt.legend()
--- tests/newton_eigenvalues.py
+++ tests/newton_eigenvalues.py
@@ -15,7 +15,7 @@ from scipy.linalg import expm, schur
 from cmdtools.estimation import Newton_Npoints, sqra
 
 #%%
-def q_doublewell(dim, beta):
+def q_doublewell(dim, sigma):
      # number of intervals in [0,1]-space (e.g. 30)
      # inverse temperature for Boltzmann (e.g. 10)
 
@@ -24,7 +24,7 @@ def q_doublewell(dim, beta):
     #using the ideas of the presentation of Donati
     m = 1.
     gamma = 1.    
-    sigma = np.sqrt(2/(beta*m*gamma))
+   # sigma = np.sqrt(2/(beta*m*gamma))
     phi = sigma**2/(2*abs(grid[0]-grid[1]))
     # potential
     u = 1/(50)*(grid**2-2)**2
@@ -38,18 +38,18 @@ def q_doublewell(dim, beta):
   
     A = np.triu(A) + np.triu(A).T
 
-    return sqra.sqra(u, A, beta, phi)
+    return sqra.sqra(u, A, 2/sigma**2, phi)
 example = q_doublewell(100,10)
 #%%  
-def obtain_q(dim=50, beta=1):
+def obtain_q(dim=50, sigma=1):
 #    infgen = random(dim, dim, density).toarray()
-    infgen = q_doublewell(dim-1, beta)
+    infgen = q_doublewell(dim-1, sigma)
 #    print(np.shape(infgen))
 #  infgen = sqra.sqra()
     infgen[np.diag_indices(dim)] = - np.sum(infgen- np.diagflat(np.diagonal(infgen)), axis=1)
     return infgen#*0.005
-def obtain_k(tau_max, step, dim=50, beta=1):#, noise=False):
-    infgen = obtain_q(dim, beta)
+def obtain_k(tau_max, step, dim=50, sigma=1):#, noise=False):
+    infgen = obtain_q(dim, sigma)
     k_matrix = np.zeros((int(tau_max/step) +1, dim, dim))
     tau_values = np.arange(0, tau_max+1, step)
     #if noise == True:
@@ -72,7 +72,7 @@ k = np.zeros((9, 6,6))
 for i in range(np.shape(k)[0]):
     k[i,:,:] = expm(tau[i]*q)   
 q_new = Newton_Npoints.Newton_N(k, 1,0)
-#beta = subspace_angles(pcca.schurvects(q,4),pcca.schurvects(q_new,4))
+#sigma = subspace_angles(pcca.schurvects(q,4),pcca.schurvects(q_new,4))
 #%%
 #def sort_schur(T, n=2):
 #    e = np.sort(np.linalg.eigvals(T))
@@ -94,8 +94,26 @@ def dummy_compare(m1,m2):
     ew1 = np.sort(np.real(np.linalg.eigvals(m1)))
     ew2 = np.sort(np.real(np.linalg.eigvals(m2)))
     return(abs(ew1-ew2), ew1,ew2)
-def find_min_error(tau_max=2, step=1, dim=50, beta=1):
-    infgen = obtain_q(dim, beta)
+#def find_min_error(tau_max=2, step=1, dim=50, sigma=1):
+#    infgen = obtain_q(dim, sigma)
+#    k_matrix = np.zeros((int(tau_max/step) +1, dim, dim))
+#    tau_values = np.arange(0, tau_max+1, step)
+#    for i in range(np.shape(k_matrix)[0]):
+#        k_matrix[i, :, :] = expm(tau_values[i]*infgen)  
+#    infgen_new = Newton_Npoints.Newton_N(k_matrix, 1.,0)
+#    err_old = (dummy_compare(infgen_new, infgen)[0])[-2] 
+#   # print(err_old)
+#    err_new = err_old +1.
+#    while abs(err_old - err_new)>10**(-8):
+#        err_old = err_new
+#        tau_max+=1
+#        k_matrix = np.vstack((k_matrix, expm(tau_max*infgen)[None]))
+#        infgen_new = Newton_Npoints.Newton_N(k_matrix, 1.,0)
+#        err_new = (dummy_compare(infgen_new, infgen)[0])[-2]
+#    return(tau_max-1, dummy_compare(infgen_new, infgen)[0], k_matrix, infgen, infgen_new)
+    #%%
+def find_min_error(tau_max=2, step=1, dim=50, sigma=1):
+    infgen = obtain_q(dim, sigma)
     k_matrix = np.zeros((int(tau_max/step) +1, dim, dim))
     tau_values = np.arange(0, tau_max+1, step)
     for i in range(np.shape(k_matrix)[0]):
@@ -104,37 +122,42 @@ def find_min_error(tau_max=2, step=1, dim=50, beta=1):
     err_old = (dummy_compare(infgen_new, infgen)[0])[-2] 
    # print(err_old)
     err_new = err_old +1.
-    while abs(err_old - err_new)>10**(-8):
-        err_old = err_new
-        tau_max+=1
-        k_matrix = np.vstack((k_matrix, expm(tau_max*infgen)[None]))
-        infgen_new = Newton_Npoints.Newton_N(k_matrix, 1.,0)
-        err_new = (dummy_compare(infgen_new, infgen)[0])[-2]
-    return(tau_max-1, dummy_compare(infgen_new, infgen)[0], k_matrix, infgen, infgen_new)
+#    while abs(err_old - err_new)>10**(-15):
+#        if err_new < err_old: 
+#            err_old = err_new
+#        else:
+#            break
+#        tau_max+=1       
+    return(dummy_compare(infgen_new, infgen)[0], k_matrix, infgen, infgen_new)
+    
     
     #%%
 #print(dummy_compare(q,q_new)[0])
 #%%
-#k , q = obtain_k(6, 1, dim=100, beta = 10, noise =False) 
-res = find_min_error(2,1,40, 1.)
-
+#k , q = obtain_k(6, 1, dim=100, sigma = 10, noise =False) 
+res = find_min_error(2,1,20, .15)
+#print(extract(res))
+lala = (dummy_compare(res[2],res[3])[0])/abs(dummy_compare(res[2],res[3])[1])
+print(lala[-2])
 #alpha, q_NEW = compare_eigenspace(k, q)
 #%%
  #test for statistics of error with these parameters
-
+#
+#for k in range(2,9):
 for j in np.arange(10,200, step=10):
     arrays = []
     for i in range(100):
-        res = find_min_error(2,1,j,5.)
+        res = find_min_error(11,1,j,.2)
         arrays.append(res)
 #    
-#    np.savez("test_newton_dim%d_b5"%j, *arrays)
+    np.savez("test_deg11_dim%(dim)d_s015"%{'dim':j}, *arrays)
+#    np.savez("test_deg%(deg)d_dim%(dim)d_s015"%{'deg':k, 'dim':j}, *arrays)
     
 #%%
-file = np.load("test_newton_dim30_b1.npz", allow_pickle=True)
-snd_eigval = []
-for i in range(100):
-    snd_eigval.append(((file["arr_%d"%i])[1])[-2])
-    #%%
-plt.hist(snd_eigval, bins = 20)
-tryk = file['arr_0'][2]
+#file = np.load("test_newton_dim30_b1.npz", allow_pickle=True)
+#snd_eigval = []
+#for i in range(100):
+#    snd_eigval.append(((file["arr_%d"%i])[1])[-2])
+#    #%%
+#plt.hist(snd_eigval, bins = 20)
+#tryk = file['arr_0'][2]
