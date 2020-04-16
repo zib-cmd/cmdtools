997eb4d8dcb0ceb7ce368b0553cad9fb6dd08b21
Renata Sechi <sechi@zib.de>
2020-04-16 13:21:35 +0200

    problem with pa?

--- src/cmdtools/estimation/picking_algorithm.py
+++ src/cmdtools/estimation/picking_algorithm.py
@@ -42,54 +42,6 @@ def picking_algorithm(traj, n):
     distances = dist.cdist(traj, traj, dist.sqeuclidean)
     
     return(traj, traj[picking_list(distances, n),:])
-    
-##%%
-    
-#def brownian(x0, n, dt, delta, out=None):
-#    x0 = np.asarray(x0)
-#
-#    # For each element of x0, generate a sample of n numbers from a
-#    # normal distribution.
-#    r = norm.rvs(size=x0.shape + (n,), scale=delta*np.sqrt(dt))
-#
-#    # If `out` was not given, create an output array.
-#    if out is None:
-#        out = np.empty(r.shape)
-#
-#    # This computes the Brownian motion by forming the cumulative sum of
-#    # the random samples. 
-#    np.cumsum(r, axis=-1, out=out)
-#
-#    # Add the initial condition.
-#    out += np.expand_dims(x0, axis=-1)
-#
-#    return out
-##%%
-#delta = 0.5
-## Total time.
-#T = 10.0
-## Number of steps.
-#N = 300
-## Time step size
-#dt = T/N
-## Initial values of x.
-#x = np.empty((2,N+1))
-#x[:, 0] = 0.0
-#
-#brownian(x[:,0], N, dt, delta, out=x[:,1:])
-#
-### Plot the 2D trajectory.
-##plt.scatter(x[0],x[1])
-##plt.show()
-##%%
-#uni = np.random.uniform(0,2, size = (100,2))
-##example
-#A, B, liste = picking_algorithm(-np.pi, np.pi, 2, 30, uni)
-###%%
-#plt.scatter(A[:, 0], A[:, 1])
-#plt.scatter(B[:, 0], B[:, 1], color = "r")
-#plt.grid()
-#plt.show()
-#plt.imshow(liste)
-#plt.colorbar()
-#plt.show()
+ 
+
+
---
+++ src/cmdtools/systems/diala_picking_algorithm/tutorial_diala_picking_algorithm.py
@@ -0,0 +1,169 @@
+
+# coding: utf-8
+
+# # Tutorial: obtaining the Koopman generator via Newton's extrapolation
+
+# This tutorial shows you how to compute the infinitesimal generator with the Newton's polynomial extrapolation.<br\>
+# First, load the modules we need. In addition to numpy and matplolib, we use the picking algorithm, Galerkin discretization for different lagtimes, Newton extrapolation, pcca+.
+
+# In[1]:
+
+
+#standard libraries
+import numpy as np
+import matplotlib.pyplot as plt
+#from cmdtools
+from cmdtools.analysis import pcca
+from cmdtools.estimation import galerkin_taus_all, Newton_Npoints, picking_algorithm
+
+
+# Load the trajectory data from  the directory and the centers for the Gaussian membership fuctions computed with the picking algorithm.<br\> We load the coordinates of the centers to make this tutorial faster. In case you want to vary the number of centers or to try by yourself how this algorthm works, uncomment the line below and comment the line that loads the centers coordinates. The picking algorithm requires a bit of time and we suggest you save the coordinates of your centers. 
+
+# In[2]:
+
+
+#load trajectory  and centres  
+diala = np.load("arr_0.npy")
+centers= np.loadtxt("p_a_351centers.txt")
+#uncomment to test the picking algorithm by yourself
+#centers = picking_algorithm.picking_algorithm(diala[::25,:], 350)[1]
+# we reccomend to sort your centers so that you can visualize them later.
+centers= centers[centers[:, 0].argsort()]
+#np.savetxt('p_a_351centers.txt', centers)
+
+
+# We visualize the position of the picked points in the state space.<br\>
+# You can see that this algorithm ensures that the sampling is localized in the region of of the state space covered by the trajectory. 
+
+# In[3]:
+
+
+plt.scatter( diala[::25, 0], diala[::25, 1], label= "diala traj")
+plt.scatter(centers[:, 0], centers[:, 1], label= "centers")
+plt.xlabel("$\Phi$ [rad]")
+plt.ylabel("$\Psi$ [rad]")
+plt.title("Dialanine and centers of the Gaussians")
+plt.xlim(-np.pi, np.pi)
+plt.ylim(-np.pi, np.pi)
+plt.legend()
+plt.show()
+
+
+# Now we have everything to compute the Koopman matrices via Galerkin discretization. We compute a set of transfer matrices for different multiples of a timestep $\tau$. So first we set $\tau$=5, but you can try to vary it as see how the results change. <br\>
+# We compute then the transfer matrix for $0,1,2,3,4$ times $\tau$. We save it in a numpy array, whose first index $n$ indicates the $n\cdot \tau$ tagtime used to compute the Koopman matrix $K(n\cdot \tau)$. <br\>
+# We set the parameter $\sigma$ for the variance of the Gaussian to $\sigma=0.09$. 
+
+# In[4]:
+
+
+Koopman_mtx, m = galerkin_taus_all.propagator_tau(diala[::5,:], centers, 0.09, 4)
+
+
+# In[5]:
+
+
+
+#%%
+def strip_bad(counts_tensor):
+    """Find the lines in which the selfoverlap of the basis functions is not 
+    good, strip those basis points from the computed transition matrix. 
+    Input:
+        
+    Output:
+        """
+    S = counts_tensor[0,:,:]
+    to_keep = []
+    for i in range(np.shape(S)[0]):
+        #if np.argmax(S[i,:]) == i:
+        if S[i,i] > 0.2:
+            to_keep.append(i)
+                      
+    #for j in range(np.shape(counts_tensor)[0]):
+     #   counts_tensor[j,:,:] = utils.rowstochastic(counts_tensor[j,:,:])  
+    temp = counts_tensor[:,to_keep,:]
+    return temp [:,:,to_keep], to_keep
+#%%
+Koopman_mtx2, centers_kept = strip_bad(Koopman_mtx)
+#%%
+for k in range(5):
+    Koopman_mtx2[k, :, :] = Koopman_mtx2[k, :, :]/np.sum(Koopman_mtx2[k, :, :], axis = 1)
+    #%%
+S_ = Koopman_mtx2[0, :, :]
+
+
+# Now we can apply the Newton's polynomial extrapolation method to the set of Koopman matrices that we computed.
+
+# In[6]:
+
+
+#estimate generator
+
+Infgen = Newton_Npoints.Newton_N(Koopman_mtx2[:4], 1., 0)
+
+
+# Last step: the dominant spectrum of the Koopman matrix $K(\tau)$ and of the generator $Q$ is used as input for PCCA+. <br\> This version of PCCA+ uses the Schur projection of the input matrix to computed the coarse graied dynamics. <br\> We also use a matrix .................................MASS MATRIX CONTINUE.................We observed obseved 4 clusters from an analysis of the Schurvalues, but you can try to vary the number of clusters and see how the results change.
+# 
+
+# In[7]:
+
+
+chi, sorted_ew_k = pcca.pcca(Koopman_mtx2[1, :, :], 4, S_)
+#visualize pcca+ Koopman matrix 
+plt.imshow( chi, aspect="auto")
+plt.title("$\chi$ from PCCA+ of the Koopman matrix")
+plt.xticks(np.arange(0,np.shape(chi)[1]))
+plt.xlabel("Schurvalue")
+plt.ylabel("center (enumerated)")
+plt.colorbar()
+plt.show()
+
+
+# In[8]:
+
+
+chi_infgen, sorted_ew_q= pcca.pcca(Infgen,4, S_)
+
+plt.imshow(chi_infgen, aspect= "auto")
+plt.colorbar()
+plt.title("$\chi$ from PCCA+ of the generator matrix")
+plt.xticks(np.arange(0,np.shape(chi)[1]))
+plt.xlabel("Schurvalue")
+plt.ylabel("center (enumerated)")
+plt.show()
+
+
+# In[9]:
+
+
+
+#visualize with center belongs to which metastable state, in addition compare the results for the 
+#PCCA+ applied to the transfer matrix and to the generator.
+colors = ["b","r","gold","g", "m", "purple"]
+centers_1 = centers[centers_kept, :]
+plt.figure(figsize=(10, 4))
+plt.subplot(121)
+for i in range(np.shape(chi_infgen)[0]):
+    plt.scatter(centers_1[i, 0], centers_1[i, 1], color = colors[np.argmax(chi_infgen[i, :])])
+plt.xlabel("$\Phi$ [rad]")
+plt.ylabel("$\Psi$ [rad]")
+plt.title("Alanine Dipeptide, 4 Metastable States, $Q^c$")
+plt.xlim(-np.pi, np.pi)
+
+plt.subplot(122)
+for i in range(np.shape(chi_infgen)[0]):
+ 
+    plt.scatter(centers_1[i, 0], centers_1[i, 1], color = colors[np.argmax(chi[i, :])])
+plt.xlabel("$\Phi$ [rad]")
+plt.ylabel("$\Psi$ [rad]")
+plt.title("Alanine Dipeptide, 4 Metastable States, $K^c$")
+plt.xlim(-np.pi, np.pi)
+plt.show()
+
+
+# The coarse grained generator can be obtained with $Q^c=\chi^{-1}Q\chi$
+
+# In[10]:
+
+
+Q_c = np.linalg.pinv(chi_infgen).dot(Infgen.dot(chi_infgen))
+
