on the way to fix the schur computation
now solving the transformed schur problem
which is also useful for other then the constant stationary distribution
works for krylov, not yet for scipy (prob because const vector not in front)

src/cmdtools/analysis/optimization.py
@@ -43,41 +43,41 @@ def indexsearch(X):
     return ind
 
 
-def optimize(X, A0, maxiter=1000):
-    """
-    find the optimal transformation A
-    such that the membership 𝛘 = XA is as crisp as possible
-    """
-    return _feasible_optimize(X, A0, maxiter)
+if False:
+    def oldoptimize(X, A0, maxiter=1000):
+        """
+        find the optimal transformation A
+        such that the membership 𝛘 = XA is as crisp as possible
+        """
+        return _feasible_optimize(X, A0, maxiter)
+
+    def _feasible_optimize(X, A0, maxiter):
+        """
+        Since the feasiblization routine fillA() in the actual optimization
+        requires the first column of X to be the one-vector.
+        We hence solve for the transformed problem 𝛘 = XA = (XT)(IA)
+        with T a matrix, I=T^-1 and (XT) has the required constant column.
+        """
+
+        iA0 = np.linalg.inv(T).dot(A0)
+
+        iA = _optimize(tX, iA0, maxiter)
+
+        # invert the previous transformation
+        A = np.dot(T, iA)
+        return A
 
 
-def _feasible_optimize(X, A0, maxiter):
-    """
-    Since the feasiblization routine fillA() in the actual optimization
-    requires the first column of X to be the one-vector.
-    We hence solve for the transformed problem 𝛘 = XA = (XT)(IA)
-    with T a matrix, I=T^-1 and (XT) has the required constant column.
-    """
-    n, m = np.shape(X)
-    T = np.identity(m)
-    T[:, 0] = np.dot(np.ones(n), X)
-    if np.isclose(T[0, 0], 0):
-        raise RuntimeError("X[:,1] must not be orthogonal to the one-vector")
-    tX = np.dot(X, T)
-    iA0 = np.linalg.inv(T).dot(A0)
-
-    iA = _optimize(tX, iA0, maxiter)
-
-    # invert the previous transformation
-    A = np.dot(T, iA)
-    return A
 
 
-def _optimize(X, A, maxiter):
+
+def optimize(X, A, maxiter=1000):
     """
     optimization of A
-    note that the feasiblization routine fillA() requires
+    - the feasiblization routine fillA() requires
     the first column of X to be the constant one-vector
+    - the optimzation criterion expects X^T D X = I
+    (where D is the stationary diagonal matrix)
     """
     x = A[1:, 1:]
     x = fmin(objective, x0=x, args=(X, A), maxiter=maxiter)

src/cmdtools/analysis/pcca.py
@@ -11,8 +11,8 @@ except ImportError:
     USE_SLEPC = False
 
 
-def pcca(T, n):
-    X = schurvects(T, n)
+def pcca(T, n, pi=None):
+    X = schurvects(T, n, pi)
     A = inner_simplex_algorithm(X)
     if n > 2:
         A = optimize(X, A)
@@ -20,11 +20,43 @@ def pcca(T, n):
     return chi
 
 
-def schurvects(T, n):
+def schurvects(T, n, pi=None):
+    """
+    compute the leading n schurvectors X
+    wrt. the scalar product induced by pi
+    (i.e. X^T D X = I with D=diag(pi))
+    """
+    if pi is None:
+        dim = np.size(T, 1)
+        pi = np.full(dim, 1/dim)
+
+    # Solve the transformed problem
+    # Xb = schur(Pb)
+    # Pb = D^1/2 P D^-1/2, X = D^-1/2 Xb
+    # Pb Xb = Xb S => PX = X S
+    # Xb^T Xb = I  => X^T D X = I
+
+    D = np.diag(np.sqrt(pi))
+    Di = np.diag(np.sqrt(1/pi))
+    Pb = D.dot(T).dot(Di)
+
     if USE_SLEPC:
-        return krylovschur(T, n)
+        Xb = krylovschur(Pb, n)
     else:
-        return scipyschur(T, n)
+        Xb = scipyschur(Pb, n)
+
+    X = Di.dot(Xb)
+    X[:, 0] = X[:, 0] * np.sign(X[0, 0]) # fix sign of first column
+    assertstructure(X, pi)
+    return X
+
+
+def assertstructure(X, pi):
+    I = np.identity(np.size(X, 1))
+    D = np.diag(pi)
+    XTDX = X.T.dot(D).dot(X)
+    assert np.all(np.isclose(X[:, 0], 1))
+    assert np.all(np.isclose(XTDX - I, 0))
 
 
 def scipyschur(T, n, massmatrix=None, onseperation="warn"):
@@ -58,7 +90,12 @@ def scipyschur(T, n, massmatrix=None, onseperation="warn"):
     return X[:, 0:n]  # use only first n vectors
 
 
-def krylovschur(A, n):
+def krylovschur(A, n, massmatrix=None, onseperation="continue"):
+    if massmatrix is not None:
+        raise NotImplementedError
+    if onseperation != "continue":
+        raise NotImplementedError
+
     from petsc4py import PETSc
     from slepc4py import SLEPc
     M = PETSc.Mat().create()
@@ -70,3 +107,26 @@ def krylovschur(A, n):
     E.solve()
     X = np.column_stack([x.array for x in E.getInvariantSubspace()])
     return X[:, :n]
+
+
+def normalizeschur(X):
+    # find the constant eigenvector corresponding to ev. 1,
+    # move it to the front and set it to 1
+    # as required by the optimization routine
+
+    X /= np.linalg.norm(X, axis=0)
+    i = np.argmax(np.abs(np.sum(X, axis=0)))
+    X[:, i] = X[:, 0]
+    X[:, 0] = 1  # TODO: check if this column is indeed constant
+
+    return X
+
+
+def normalizeschur2(X):
+    n, m = np.shape(X)
+    T = np.identity(m)
+    T[:, 0] = np.dot(np.ones(n)/np.sqrt(n), X)
+    if np.isclose(T[0, 0], 0):
+        raise RuntimeError("X[:,1] must not be orthogonal to the one-vector")
+    tX = np.dot(X, T)
+    return tX

tests/test_pcca.py
@@ -45,8 +45,8 @@ def test_krylovschur(n=30, m=5, N=100):
         for i in range(N):
             A = utils.randompropagator(n, reversible=False)
             try:
-                S = pcca.scipyschur(A, m)
-            except AssertionError:
+                S = pcca.scipyschur(A, m, onseperation="error")
+            except RuntimeError:
                 continue
             K = pcca.krylovschur(A, m)
             R = np.linalg.matrix_rank(np.concatenate([S, K], axis=1), tol=1e-6)