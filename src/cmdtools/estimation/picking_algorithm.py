817eb39da2152a8d980d783dacfe10fb1d6f532b
Alexander Sikorski <sikorski@zib.de>
2020-06-10 12:33:44 +0200

    picking_algorithm: remove old, change return value

--- src/cmdtools/estimation/picking_algorithm.py
+++ src/cmdtools/estimation/picking_algorithm.py
@@ -3,77 +3,32 @@ import numpy as np
 import scipy.spatial.distance as dist
 
 
-def picking_list(d, n):
-    """
-    Picking algorithm (Durmaz, 2016)
+def picking_algorithm(X, n):
+    """Picking algorithm (Durmaz, 2016)
+
     Pick out n points such that the respective minimal distance
-    to the other picked points is maximized.    Parameters
-    ----------
-    d : array
-        Matrix containing the pairwise distances of the points.
-    n : int
-        Number of points to pick.    Returns
-    -------
-    inds : list[int]
-        List of indices of the chosen points.    """
+    to the other picked points is maximized.
 
-    d = d.copy()
-    q1 = 0
-    q2 = np.argmax(d[q1, :])
-    qs = [q1, q2]
-    for k in range(1, n):
-        ss = np.argmin(d[qs, :], axis=0)
-        mins = list(d[qs[j], s] for s, j in enumerate(ss))
-        q = np.argmax(mins)
-        qs.append(q)
-    return qs
-
-
-def picking_algorithm(traj, n):
-    """
-    Return coordinates of the points picked from the
-    picking_list algorithm, using  Euclidean distances.
     Parameters
-    ------------
-    traj = 2d array
-         Trajectory coordinates
-    n = int
-    Number of points to pick
-
+    ----------
+    X : (N x d) matrix
+        Matrix of N d-dimensional points to pick from
+    n : int
+        Number of points to pick.
+    Returns
+    -------
+    P : (n x d) matrix
+        Matrix containing the n chosen points
+    qs : list[int]
+        List of the original indices of the chosen points.
+    d : (n x n) matrix
+        Distances between the chosen points
     """
-    distances = dist.cdist(traj, traj, dist.sqeuclidean)
-
-    return(traj, traj[picking_list(distances, n), :])
-
-
-def picking_fast(X, n):
-    """
-    X, matrix: (n X d) matrix of n d-dimensional points to pick from
-    """
-    q = 0
-    d = dist.cdist(X[[0], :], X)  # distance matrix, building over time
-    q = np.argmax(d[0, :])        # farthest point from X[0,:]
-    qs = [0, q]                   # list of picked points
-    for k in range(1, n):
-        d = np.vstack((d, dist.cdist(X[[q], :], X)))
-        mins = np.min(d, axis=0)
-        q = np.argmax(mins)
-        qs.append(q)
-    return X[qs, :]
-
-
-def picking_prealloc(X, n):
-    """
-    X, matrix: (n X d) matrix of n d-dimensional points to pick from
-    """
-
     d = np.empty((n, np.size(X, 0)))
-    q = 0
+    d[0, :] = dist.cdist(X[[0], :], X)
     qs = [0]
-    for k in range(0, n):
+    for k in range(1, n):
+        q = np.argmax(np.min(d[:k, :], axis=0))
         d[k, :] = dist.cdist(X[[q], :], X)
-        mins = np.min(d[:k+1, :], axis=0)
-        q = np.argmax(mins)
         qs.append(q)
-    # return X[qs, :], qs, d[:, qs]   # TODO test this
-    return X[qs, :]
+    return X[qs, :], qs, d[:, qs]
--- tests/test_picking.py
+++ tests/test_picking.py
@@ -1,34 +1,13 @@
-import cmdtools.estimation.picking_algorithm as pick
+from cmdtools.estimation.picking_algorithm import picking_algorithm
+import scipy.spatial.distance as dist
 import numpy as np
 
 
-def test_bench_prealloc(benchmark, n=100, m=10, d=3):
-    X = np.random.rand(n, d)
-    benchmark(pick.picking_prealloc, X, m)
-
-
-def test_bench_fast(benchmark, n=100, m=10, d=3):
-    X = np.random.rand(n, d)
-    benchmark(pick.picking_fast, X, m)
-
-
-def test_bench_old(benchmark, n=100, m=10, d=3):
-    X = np.random.rand(n, d)
-    benchmark(pick.picking_algorithm, X, m)
-
-# n=100,  m=100, d=2: 20x faster (worst case)
-# n=1000, m=100, d=2: 242x
-# n=1000, m=35,  d=2: 3000x (ratio of diala, but 10x lower n => expect 30000x)
-
-# old algorithm is O(n^2)
-# new one is O(n*m)
-# and has a better constant since were not dealing with the intermediate list
-
-
-def test_comparepick(n=100, m=10, d=3):
-    X = np.random.rand(n, d)
-    _, p1 = pick.picking_algorithm(X, m)
-    p2 = pick.picking_fast(X, m)
-    p3 = pick.picking_prealloc(X, m)
-    assert np.all(np.isclose(p1, p2))
-    assert np.all(np.isclose(p1, p3))
+def test_picking():
+    n = 10
+    m = 5
+    X = np.repeat(np.array([range(n)]).T, axis=1, repeats=2)
+    P, inds, d = picking_algorithm(X, m)
+    assert (P == X[inds, :]).all
+    assert (d == dist.cdist(P, P)).all()
+    assert np.amin(d + np.identity(m) * 10) > 2
