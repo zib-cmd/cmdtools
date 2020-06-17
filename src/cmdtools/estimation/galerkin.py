f2bc900b883eb02bdc4ca45167615ec619b85e6f
Alexander Sikorski <sikorski@zib.de>
2020-06-17 12:20:28 +0200

    fix wrong bandwidth heuristic
    we squared the squared distsances, this is wrong
    we can just use the median of the sqd
    since it is the square of the median of the distances

--- src/cmdtools/estimation/galerkin.py
+++ src/cmdtools/estimation/galerkin.py
@@ -77,29 +77,28 @@ def find_bandwidth(sqd, percentile=50):
     """
 
     Given the square-distance matrix d_ij = (x_i - x_j)^2
-    compute the standard deviation s for a Gaussian kernel
-    k(x_i,x_j) = exp(-1/h d_ij) with bandwidth parameter h = 2s^2.
-    We have h = med^2 / log n based on the intuition that we want
-    sum_j k(x_i, x_j) ≈ n exp(-1/h med^2) = 1
-    where n is the number of points and med the pairwise median distance
+    compute the standard deviation s for the Gaussian kernel
+    k(x_i,x_j) = exp(-1/(2s^2) d_ij).
+    The heuristic is based on the assumption that we want
+    sum_j k(x_i, x_j) ≈ n exp(-1/(2s^2) med^2) = 1 and hence
+    (2s^2) = med^2 / log n  where n is the number of points and
+    med the pairwise median distance.
 
-    Percentile allows to shift from the median to an arbitrary percentage and
+    percentile allows to shift from the median to an arbitrary percentage and
     thus influences how many points have an influence on the bandwith.
 
-    Reference:
-    "Stein Variational Gradient Descent:
-    A General Purpose Bayesian Inference Algorithm",
-    Qiang Liu and Dilin Wang (2016).
+    Heuristic taken from "Stein Variational Gradient Descent [...],
+    Qiang Liu and Dilin Wang (2016)".
 
     Input:
         sqd: matrix, pairwise squared-distances of the samples
-        percentile: int [0,100], amount of samples to be in the "horizon" of h
+        percentile: float [0,100], percentile to use instead of median
 
      Output:
          sigma: float, the standard deviation of the Gaussian
     """
 
     n = np.size(sqd, 1)
-    h = np.percentile(sqd, percentile)**2 / np.log(n)
+    h = np.percentile(sqd, percentile) / np.log(n)
     sigma = np.sqrt(h/2)
     return sigma
