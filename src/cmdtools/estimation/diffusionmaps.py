b923ab5ea157017889e49da3a6e5a05afc0e8597
Alexander Sikorski <sikorski@zib.de>
2020-06-30 15:33:39 +0200

    add Distance class
    which allows replacing scipy pairwise distances
    with nn distances

--- src/cmdtools/estimation/diffusionmaps.py
+++ src/cmdtools/estimation/diffusionmaps.py
@@ -6,13 +6,45 @@ import sklearn.neighbors as neighbors
 from cmdtools.utils import rowstochastic
 
 
+# TODO: we also have distances in picking/galerkin - unite the code
+class Distances:
+    def __init__(self, metric='sqeuclidean'):
+        self.metric = metric
+
+    def dist(self, X, Y=None):
+        if Y is X or Y is None:
+            d = scidist.pdist(X, self.metric)
+            return scidist.squareform(d)
+        else:
+            return scidist.cdist(X, Y, self.metric)
+
+    def sqdist(self, X, Y=None):
+        d = self.dist(X, Y)
+        d = d ** 2 if self.metric != 'squeclidean' else d
+        return d
+
+
+class NNDistances(Distances):
+    def __init__(self, k=1, *args, **kwargs):
+        self.k = k
+        super().__init__(*args, **kwargs)
+
+    def dist(self, X, Y=None):
+        if Y is not None:
+            warnings.warn("NN distances not supported for X!=Y, falling back")
+            return super().dist(X, Y)
+        d = neighbors.kneighbors_graph(
+            X, self.k, mode='distance', metric=self.metric)
+        return np.array(d.todense()) # since we cant deal with sparse so far
+
+
 class DiffusionMaps:
 
-    def __init__(self, X, sigma=1, alpha=1, metric='sqeuclidean', n=1):
+    def __init__(self, X, sigma=1, alpha=1, distances=Distances(), n=1):
         self.X = X
         self.sigma = sigma
         self.alpha = alpha
-        self.metric = metric
+        self.distances = distances
         self.n = n
 
         self.diffusion_matrix()
@@ -20,21 +52,20 @@ class DiffusionMaps:
 
     def diffusion_matrix(self):
         self.P, self.q = diffusion_matrix(
-            self.X, self.sigma, self.alpha, self.metric)
+            self.X, self.sigma, self.alpha, self.distances)
 
     def diffusionmaps(self):
         self.dms, self.evals, self.evecs = diffusionmaps(self.P, self.n)
 
     def oos_extension(self, Xnew):
         return oos_extension(
-            Xnew, self.X, self.metric, self.sigma, self.alpha,
+            Xnew, self.X, self.distances, self.sigma, self.alpha,
             self.q, self.dms, self.evals)
 
 
-def diffusion_matrix(X, sigma, alpha, metric):
+def diffusion_matrix(X, sigma, alpha, distances):
     # unnormalized kernel
-    dist = scidist.squareform(scidist.pdist(X, metric))  # TODO: approximate with nearest neighbours
-    sqd = dist if metric == 'sqeuclidean' else dist**2
+    sqd = distances.sqdist(X)
     K = np.exp(-sqd / (2 * sigma**2))
 
     # pre normalization
@@ -59,44 +90,21 @@ def diffusionmaps(P, n):
     return dms, evals, evecs
 
 
-def oos_extension(Xnew, Xold, metric, sigma, alpha, q, dms, evals):
-    dist = scidist.cdist(Xnew, Xold, metric)
-    sqd = dist if metric == 'sqeuclidean' else dist**2
+def oos_extension(Xnew, Xold, distances, sigma, alpha, q, dms, evals):
+    sqd = distances.sqdist(Xnew, Xold)
+
     Knew = np.exp(-sqd / (2 * sigma**2))
-
     qnew = np.power(np.sum(Knew, axis=1), -alpha)
-
     Kh = np.diag(qnew).dot(Knew).dot(np.diag(q))
     Pnew = rowstochastic(Kh)
 
     dmsnew = Pnew.dot(dms) / evals[None, :]
+    dmsnew = np.real(dmsnew)
     return dmsnew
 
 
 def test_diffusionmaps():
-    X = np.random.rand(100, 100)
+    X = np.random.rand(100, 5)
     dm = DiffusionMaps(X)
+    dm2 = DiffusionMaps(X, distances=NNDistances(20))
     assert np.allclose(dm.oos_extension(X), dm.dms)
-
-
-def nearest_neightbor_dists(X, k, metric):
-    return neighbors.kneighbors_graph(X, k, mode='distance', metric=metric)
-
-
-def superdist(X, Y = None, metric='squeclidean', squared=True, knn=None):
-    if Y is X:
-        Y = None
-    if knn not None:
-        if Y not None:
-            raise Error("Nearest neighbor not defined for X neq Y")
-        d = neighbors.kneighbors_graph(X, knn, mode='distance', metric=metric)
-    else:
-        if Y is None:
-            d = scidist.squareform(scidist.pdist(X, metric))
-        else:
-            d = scidist.cdist(X, Y, metric)
-
-    if squared and metric is not 'squeclidean':
-        d = d**2
-
-    return d
