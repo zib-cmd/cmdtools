3723f32f718d59c650362452673e29e7a04a1c31
Alexander Sikorski <sikorski@zib.de>
2020-09-15 12:50:15 +0200

    test_picking: fix metric in the test
    to fit the new sqeuclidean default

--- tests/test_picking.py
+++ tests/test_picking.py
@@ -9,7 +9,7 @@ def test_picking():
     X = np.repeat(np.array([range(n)]).T, axis=1, repeats=2)
     P, inds, d = picking_algorithm(X, m)
     assert np.allclose(P, X[inds, :])
-    assert np.allclose(d, dist.cdist(X, P))
+    assert np.allclose(d, dist.cdist(X, P, metric='sqeuclidean'))
 
 
 def test_picking_bench(benchmark):
