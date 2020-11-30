db65412976f4acdd482d1840fc3b7f727d71c06d
Alexander Sikorski <sikorski@zib.de>
2020-11-30 18:34:51 +0100

    diffusion: remove old code, adjust test

--- src/cmdtools/systems/diffusion.py
+++ src/cmdtools/systems/diffusion.py
@@ -1,59 +1,6 @@
 import numpy as np
 from ..estimation import sqra
 
-
-class DiffusionProcess:
-    """ Representation of the diffusion process
-    dX = -∇V(X) dt + √(2ε) dW """  # TODO: check if sqrt is right here
-    def __init__(self, V, epsilon=1):
-        self.V = V
-        self.epsilon = epsilon
-
-    def stationary_dist_nn(self, x):
-        """ Evaluate the non-normalized Boltzmann distribution at x """
-        return np.exp(-self.beta * self.V(x))
-
-    @property
-    def beta(self):
-        return 2 / self.epsilon
-
-    @beta.setter
-    def beta(self, b):
-        self.epsilon = beta_to_epsilon(b)
-
-
-class DoubleWell(DiffusionProcess):
-    def __init__(self):
-        def potential(x):
-            return (x**2 - 1)**2 / 4
-        DiffusionProcess.__init__(self, potential, beta_to_epsilon(1))
-
-    xmin = -2.5
-    xmax = 2.5
-    dimension = 1
-
-
-class ThreeHolePotential(DiffusionProcess):
-    """ The Three-Hole Potential from 2006 - Metzner, Schütte, Vanden-Eijnden -
-    Illustration of transition path theory """
-    def __init__(self):
-        def potential(xy):
-            x = xy[0]
-            y = xy[1]
-            return 3 * np.exp(-x**2 - (y-1/3)**2) \
-                - 3 * np.exp(-x**2 - (y-5/3)**2) \
-                - 5 * np.exp(-(x-1)**2 - y**2) \
-                - 5 * np.exp(-(x+1)**2 - y**2) \
-                + .2 * x**4 + .2 * (y-1/3)**4
-
-        DiffusionProcess.__init__(self, potential, beta_to_epsilon(1.67))
-
-    dimension = 2
-    xmin = -2
-    xmax = 2
-    ymin = -1.5
-    ymax = 2.5
-
 class AbstractDiffusion:
     def __init__(self, nx, ny, xlims, ylims, beta=1, phi=1):
 
--- tests/test_systems.py
+++ tests/test_systems.py
@@ -1,17 +1,9 @@
 from cmdtools.systems import diffusion
 
 
-def test_diffusion():
-    diffusion.DiffusionProcess(lambda x: x)
-    diffusion.DiffusionProcess(lambda x: x, epsilon=1)
-
-
 def test_doublewell():
     d = diffusion.DoubleWell()
-    d.stationary_dist_nn(1)
 
-
-def test_threehole():
-    d = diffusion.ThreeHolePotential()
-    d.beta = 1.6
-    d.stationary_dist_nn([0, 0])
+def test_tripplewell():
+    d = diffusion.TripleWell()
+    d.Q
