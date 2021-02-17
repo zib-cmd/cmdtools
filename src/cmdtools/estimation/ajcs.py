3c33de3a693a0291ee3d8db4801eec84b0cba829
Alexander Sikorski <sikorski@zib.de>
2021-02-17 18:07:57 +0100

    ajc: replace old jumpkernel

--- src/cmdtools/estimation/ajcs.py
+++ src/cmdtools/estimation/ajcs.py
@@ -26,58 +26,11 @@ class AJCS():
 
     def compute(self):
         self.qt, self.qi = self.qtilde(self.Q)
-        self.k, self.H, self.S = self.jumpkernel(self.qt, self.qi, self.dt)
+        self.k, self.S = self.jumpkernel(self.qt, self.qi, self.dt)
 
     @staticmethod
     def jumpkernel(qt, qi, dt):
         """ compute the galerkin discretization of the jump kernel eq[50] """
-
-        nx, nt = np.shape(qi)
-        s = np.exp(-qi * dt[None, :])  # (nx, nt)
-
-        H = np.zeros((nx, nt, nt))
-        for i in range(nx):
-            for k in range(nt):
-                prod = np.append(1, np.cumprod(s[i, k+1:-1]))
-                H[i, k, k+1:] = (1-s[i, k]) * (1-s[i, k+1:]) * prod
-                H[i, k, k] = s[i, k] + dt[k] * qi[i, k] - 1
-
-        # J = np.einsum('k, ijl, ik, ikl -> ikjl', 1/dt, qt, 1/qi, H)
-        J = np.empty((nt, nt), dtype=object)
-        S = np.zeros((nt, nt, nx))
-        for k in range(nt):
-            for l in range(nt):
-                S[k, l, :] = (1/dt[k]) * H[:, k, l] / (qi[:, k])
-                J[k, l] = sp.diags(S[k, l, :]).dot(qt[l])
-                # is  csr_scale_rows faster?
-        return J, H, S
-
-    @staticmethod
-    def qtilde(Qs):
-        """ given an array of rate matrices compute the jump chain matrix
-        qt[i,j,t] = q^tilde_ij(t) : the row normalized jump matrix [eq. 14]
-        qi[i,t] = q_i(t): the outflow rate [eq. 6]
-        """
-
-        qt = [sp.dok_matrix(Q) for Q in Qs]
-        nt = len(qt)
-        nx = qt[0].shape[0]
-        qi = np.zeros((nt, nx))
-        for k in range(nt):
-            qt[k][range(nx), range(nx)] = 0
-            qi[k, :] = qt[k].sum(axis=1).A[:, 0]
-            qt[k] = sp.diags(1/qi[k, :]).dot(qt[k])
-            z = np.where(qi[k, :] == 0)           # special case q_i = 0 => q_ij = kron_ij
-            qt[k][z[0], :] = 0
-            qt[k][z[0], z[0]] = 1
-
-        return qt, qi.T
-
-    @staticmethod
-    def jumpkernel2(qt, qi, dt):
-        """ compute the galerkin discretization of the jump kernel eq[50] """
-
-        qi = qi.T
 
         nt, nx = np.shape(qi)
         s = np.exp(- dt[:, None] * qi)  # (nt, nx)
@@ -100,6 +53,27 @@ class AJCS():
             for l in range(nt):
                 J[k, l] = sp.diags(S[k, l, :]).dot(qt[l])  # is  csr_scale_rows faster?
         return J, S
+
+    @staticmethod
+    def qtilde(Qs):
+        """ given an array of rate matrices compute the jump chain matrix
+        qt[t,i,j] = q^tilde_ij(t) : the row normalized jump matrix [eq. 14]
+        qi[t,i] = q_i(t): the outflow rate [eq. 6]
+        """
+
+        qt = [sp.dok_matrix(Q) for Q in Qs]
+        nt = len(qt)
+        nx = qt[0].shape[0]
+        qi = np.zeros((nt, nx))
+        for k in range(nt):
+            qt[k][range(nx), range(nx)] = 0
+            qi[k, :] = qt[k].sum(axis=1).A[:, 0]
+            qt[k] = sp.diags(1/qi[k, :]).dot(qt[k])
+            z = np.where(qi[k, :] == 0)           # special case q_i = 0 => q_ij = kron_ij
+            qt[k][z[0], :] = 0
+            qt[k][z[0], z[0]] = 1
+
+        return qt, qi
 
     @property
     def km(self):
--- tests/test_ajc.py
+++ tests/test_ajc.py
@@ -1,22 +1,16 @@
 from cmdtools.systems import diffusion
 from cmdtools.estimation import ajcs
 import numpy as np
+from scipy.linalg import expm
 
 
 def test_ajc():
     Q = diffusion.DoubleWell().Q
-    a = ajcs.AJCS([Q, Q], [1, 1])
+    a = ajcs.AJCS(np.repeat(Q, 10), np.repeat(0.1, 10))
+
+    assert((expm(Q) - a.koopman() < .1).all())
 
     g = np.zeros((a.nt, a.nx))
     g[0, :] = np.nan
     g[1, 1] = 1
     a.space_time_committor(g)
-
-
-def test_jumpkernel2():
-    Q = diffusion.DoubleWell().Q
-    a = ajcs.AJCS([Q, Q], [1, 1])
-
-    j1 = np.vstack([i.todense() for i in np.reshape(a.jumpkernel(a.qt, a.qi, a.dt)[0], 4)])
-    j2 = np.vstack([i.todense() for i in np.reshape(a.jumpkernel2(a.qt, a.qi, a.dt)[0], 4)])
-    assert(np.isclose(j1,j2).all())
